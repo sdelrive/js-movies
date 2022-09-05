dec6bb8172127217900cde95912eaff4e92e5464
santiago_del_rivero <santiago.delrivero@cencosud.com>
2022-09-04 22:07:15 -0300

    agregado boton eliminar

--- javacript_Proyecto-main/app/js/main.js
+++ javacript_Proyecto-main/app/js/main.js
@@ -1,131 +1,35 @@
-
-// let peliculas = [];
 let peliculas = [
-{ id: 1, nombre: "Batman", genero: "Superheroes", director: "Nolan", año: "2005", estrellas: "", img:("https://pics.filmaffinity.com/El_caballero_oscuro-102763119-large.jpg")},
-
-{ id: 2, nombre: "Intelestelar", genero: "Ciencia Ficcion", director: "Nolan", año: "2011", estrellas: "", img:("https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg")},
-{ id: 3, nombre: "Joker", genero: "Suspenso", director: "Todd Phillips", año: "2019", estrellas: "", img:("https://pics.filmaffinity.com/Joker-790658206-large.jpg")},
-{ id: 4, nombre: "The Green Knight", genero: "Fantasía", director: "David Lowery", año: "2021", estrellas: "", img:("https://upload.wikimedia.org/wikipedia/en/0/09/The_Green_Knight_poster.jpeg")},
-{ id: 5, nombre: "The Shinning", genero: "Suspenso", director: "Kubrick", año: "1980", estrellas: " ", img:("https://s3.amazonaws.com/static.rogerebert.com/uploads/movie/movie_poster/the-shining-1980/large_zc5y5OwKSV9MDXpfWxwrOjyRHsq.jpg")},
-{ id: 6, nombre: "Parasite", genero: "Suspenso", director: "Joon-ho", año: "2019", estrellas: "", img:("https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg")},
-
-{ id: 7, nombre: "Dune", genero: "Ciencia Ficción", director: "Villeneuve", año: "2021", estrellas: "", img:("https://pics.filmaffinity.com/Dune-209834814-large.jpg")},
-
-{id: 8, nombre: "Tolkien", genero: "Ciencia Ficción", director: "Karukoski", año: "2019", estrellas: "", img:("https://pics.filmaffinity.com/Tolkien-240611295-large.jpg")},
-
-{id: 9, nombre: "Star Wars: Episodio IV", genero: "Ciencia Ficción", director: "George Lucas", año: "1977", estrellas: "", img:("https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg")},
-
-{id: 10, nombre: "Star Wars: Episodio V", genero: "Ciencia Ficción", director: "George Lucas", año: "1980", estrellas: "", img:("https://es.web.img3.acsta.net/pictures/bzp/01/25802.jpg")},
-
-{id: 11, nombre: "Star Wars: Episodio VI", genero: "Ciencia Ficción", director: "George Lucas", año: "1983", estrellas: "", img:("https://es.web.img3.acsta.net/medias/nmedia/18/78/91/11/20136910.jpg")},
-
-{id: 12, nombre: "Jojo Rabbit", genero: "Comedia", director: "Taika Waititi", año: "2019", estrellas: "", img:("https://pics.filmaffinity.com/Jojo_Rabbit-509852787-large.jpg")},
-
-{id: 12, nombre: "El Conjuro", genero: "Terror", director: "James Wan", año: "2013", estrellas: "", img:("https://mx.web.img3.acsta.net/pictures/19/03/20/19/26/1988298.jpg")},
-
-{id: 13, nombre: "El Conjuro 2", genero: "Terror", director: "James Wan", año: "2016", estrellas: "", img:("https://mx.web.img3.acsta.net/pictures/17/08/07/07/27/346398.jpg")},
-
-{id: 14, nombre: "El Conjuro 3", genero: "Terror", director: "James Wan", año: "2021", estrellas: "", img:("https://www.cartelera.com.uy/imagenes_espectaculos/moviedetail13/30222.jpg")},
-
-{id: 15, nombre: "Viernes 13", genero: "Terror", director: "Cunningham", año: "1980", estrellas: "", img:("https://pics.filmaffinity.com/Viernes_13-910183131-large.jpg")},
-
-
-
-];
-
-
-let misPeliculas = [
-
+    { id: 1, nombre: "Batman", genero: "Superheroes", director: "Nolan", año: "2005", estrellas: "", img:("https://pics.filmaffinity.com/El_caballero_oscuro-102763119-large.jpg")},
+    
+    { id: 2, nombre: "Intelestelar", genero: "Ciencia Ficcion", director: "Nolan", año: "2011", estrellas: "", img:("https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg")},
+    { id: 3, nombre: "Joker", genero: "Suspenso", director: "Todd Phillips", año: "2019", estrellas: "", img:("https://pics.filmaffinity.com/Joker-790658206-large.jpg")},
+    { id: 4, nombre: "The Green Knight", genero: "Fantasía", director: "David Lowery", año: "2021", estrellas: "", img:("https://upload.wikimedia.org/wikipedia/en/0/09/The_Green_Knight_poster.jpeg")},
+    { id: 5, nombre: "The Shinning", genero: "Suspenso", director: "Kubrick", año: "1980", estrellas: " ", img:("https://s3.amazonaws.com/static.rogerebert.com/uploads/movie/movie_poster/the-shining-1980/large_zc5y5OwKSV9MDXpfWxwrOjyRHsq.jpg")},
+    { id: 6, nombre: "Parasite", genero: "Suspenso", director: "Joon-ho", año: "2019", estrellas: "", img:("https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg")},
+    
+    { id: 7, nombre: "Dune", genero: "Ciencia Ficción", director: "Villeneuve", año: "2021", estrellas: "", img:("https://pics.filmaffinity.com/Dune-209834814-large.jpg")},
+    
+    {id: 8, nombre: "Tolkien", genero: "Ciencia Ficción", director: "Karukoski", año: "2019", estrellas: "", img:("https://pics.filmaffinity.com/Tolkien-240611295-large.jpg")},
+    
+    {id: 9, nombre: "Star Wars: Episodio IV", genero: "Ciencia Ficción", director: "George Lucas", año: "1977", estrellas: "", img:("https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg")},
+    
+    {id: 10, nombre: "Star Wars: Episodio V", genero: "Ciencia Ficción", director: "George Lucas", año: "1980", estrellas: "", img:("https://es.web.img3.acsta.net/pictures/bzp/01/25802.jpg")},
+    
+    {id: 11, nombre: "Star Wars: Episodio VI", genero: "Ciencia Ficción", director: "George Lucas", año: "1983", estrellas: "", img:("https://es.web.img3.acsta.net/medias/nmedia/18/78/91/11/20136910.jpg")},
+    
+    {id: 12, nombre: "Jojo Rabbit", genero: "Comedia", director: "Taika Waititi", año: "2019", estrellas: "", img:("https://pics.filmaffinity.com/Jojo_Rabbit-509852787-large.jpg")},
+    
+    {id: 12, nombre: "El Conjuro", genero: "Terror", director: "James Wan", año: "2013", estrellas: "", img:("https://mx.web.img3.acsta.net/pictures/19/03/20/19/26/1988298.jpg")},
+    
+    {id: 13, nombre: "El Conjuro 2", genero: "Terror", director: "James Wan", año: "2016", estrellas: "", img:("https://mx.web.img3.acsta.net/pictures/17/08/07/07/27/346398.jpg")},
+    
+    {id: 14, nombre: "El Conjuro 3", genero: "Terror", director: "James Wan", año: "2021", estrellas: "", img:("https://www.cartelera.com.uy/imagenes_espectaculos/moviedetail13/30222.jpg")},
+    
+    {id: 15, nombre: "Viernes 13", genero: "Terror", director: "Cunningham", año: "1980", estrellas: "", img:("https://pics.filmaffinity.com/Viernes_13-910183131-large.jpg")},
+    
+    
     
     ];
 
-
-// Aquí guardaré las películas en mi lista
-let catalogoPeliculas = new CatalogoPeliculas(peliculas);
-let catalogoMisPeliculas = new CatalogoPeliculas(misPeliculas);
-
-// menu();
-
-function menu(){
-
-    let opcion = " ";
-    opcion = prompt(`Ingrese el número de opción deseada: 
-        1. Ver películas disponibles
-        2. Buscar películas
-        3. Añadir pelicula a Mis Peliculas
-        4. Ver Mis Peliculas
-        5. Eliminar de Mis Películas
-        `);
-
-    switch (opcion){
-        case "1": listarPeliculas();
-        break;
-        case "2": buscarPeliculas();
-        break;
-        case "3": añadirMisPeliculas();
-        break;
-        case "4": listarMisPeliculas();
-        break;
-        case "5": modificarMisPeliculas();
-        break;
-
-        default: alert("Dígito incorrecto");
-        break;
-        menu();
-        }
-
-
-}
-
-
-    // catalogo de peliculas
-function listarPeliculas(){
-
-    catalogoPeliculas.listarMisPeliculas();
-    menu();
-
-}
-
-function buscarPeliculas(){
-    let name = prompt("Ingrese la pelicula que desea buscar.")
-    catalogoPeliculas.buscarMisPeliculas(name);
-
-}
-
-
-    //catalogo de MIS peliculas
-
-
-function añadirMisPeliculas(){
-
-    // let nombre = prompt("Ingrese nombre de película.")
-    // let genero = prompt("Ingrese genero de película.")
-    // let director = prompt("Ingrese director de película.")
-    // let año = prompt("Ingrese año de película.")
-    // let estrellas = prompt("Ingrese su calificación.")
-
-
-    miPelicula = new Pelicula(catalogoMisPeliculas.cuantasHay()+1, nombre, genero, director, año, estrellas );
-
-    catalogoMisPeliculas.agregarMisPeliculas(miPelicula);
-    alert("Su pelicula ha sido agregada con éxito.")
-    menu();
-}
-
-
-function listarMisPeliculas(){
-    catalogoMisPeliculas.listarMisPeliculas();
-    menu();
-}
-
-
-
-
-function modificarMisPeliculas(){
-   let name = prompt("Ingrese el nombre completo de la película a eliminar");
-
-   catalogoMisPeliculas.modificarMisPeliculas(name);
-   
-menu();
-
-}
+    let misPelis = [
+        ];
--- javacript_Proyecto-main/app/js/script.js
+++ javacript_Proyecto-main/app/js/script.js
@@ -4,22 +4,12 @@ const miListado = document.getElementById("miListado");
 const lista = document.getElementById("listado");
 const btns = document.getElementsByClassName("button");
 
-
-
-misPelis = catalogoMisPeliculas;        // No sé por qué tengo que hacer esto. Si pongo catalogoMisPeliculas directamente en el for de la función botonListener, me dice que tengo que iniciar catalogoMisPeliculas primero. Por eso lo defini asi y funcionó.
-// localStorage.setItem("MIS PELIS", []);
-// let miListaJSON = JSON.parse(localStorage.getItem("mis pelis")).peliculas;
-
-
 listaUsuario(); //Pega el nombre de usuario en "Lista de ${userName}"
-
-
 pegarLista(peliculas);
 
-
-//Añado los eventos a los botones de navegación.
+    //Añado los eventos a los botones de navegación.
 miListado.addEventListener('click',()=>{
-    pegarMiLista(misPelis.peliculas);   //Le pego un array de objetos
+    pegarMiLista(misPelis);   //Le pego un array de objetos
 });
 
 lista.addEventListener('click',()=>{
@@ -28,9 +18,7 @@ lista.addEventListener('click',()=>{
 });
 
 
-//--------------------------------
- 
-       /* ESTA FUNCIÓN PEGA LAS LISTAS DE PELICULAS EN EL HTML*/
+       // ESTA FUNCIÓN PEGA LAS LISTAS DE PELICULAS EN EL HTML
 function pegarLista(catalogo){
         layout.innerHTML = " ";     //Primero borro todo lo que haya
         
@@ -60,13 +48,8 @@ function pegarLista(catalogo){
         let string = `btn${i}`;
         botonListener(string,element);
     };
-      //Eliminar repetidos.
-      //catalogoMisPeliculas.peliculas es un array de objetos.
-
 }
-       
-//Pegar Mi Lista 
-// catalogo debe ser un array de objetos.
+
 function pegarMiLista(catalogo){
     layout.innerHTML = " ";     //Primero borro todo lo que haya
 
@@ -89,39 +72,48 @@ function pegarMiLista(catalogo){
             
     }
 
-    for(let i=1;i<misPelis.peliculas.length;i++){
-        let element = misPelis.peliculas[i-1];
+    for(let i=1;i<=misPelis.length;i++){
+        let element = misPelis[i-1];
         let string = `rmvFilm${i}`;
-        // eventEliminarMiLista(string,element);
+        eventEliminarMiLista(string,element);
     };
 
 }
 
+function eventEliminarMiLista(string,element){
+    let btn = document.getElementById(string);
+    btn.addEventListener('click', () =>{
+        // console.log(element)
+        misPelis = misPelis.filter(peli => 
+            peli.id != element.id)
 
-//----
-        
+        pegarMiLista(misPelis);   //Le pego un array de objetos
+
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'La película ha sido eliminada de tu lista.',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    })
+
+}
             // Funcion de listener de los botones.
 function botonListener(string,element){
     let btn = document.getElementById(string);
     btn.addEventListener('click', () =>{
 
-            miPelicula = new Pelicula(element.id, element.nombre, element.genero, element.director, element.año, element.estrellas,element.img );
-            misPelis.agregarMisPeliculas(miPelicula);
-            let catalogoMisPeliculas = new CatalogoPeliculas(misPelis);
-
-            let catalogoMPJson = JSON.stringify(misPelis);
-            localStorage.setItem("mis pelis",catalogoMPJson);
-
-            let miListaJSON = JSON.parse(localStorage.getItem("mis pelis")).peliculas;
-
+            // let catalogoMPJson = JSON.stringify(misPelis);
+            // localStorage.setItem("mis pelis",catalogoMPJson);
+            // let miListaJSON = JSON.parse(localStorage.getItem("mis pelis")).peliculas;
             // if(localStorage.getItem("mis pelis")){
             //     console.log("alo: ", JSON.parse(localStorage.getItem("mis pelis")))
             //     // let catalogoMisPeliculas = new CatalogoPeliculas(JSON.parse(localStorage.getItem("mis pelis")));
             // }
             // let pelis = JSON.parse(localStorage.getItem("mis pelis"));
 
-            
-
+            misPelis.push(element);
             agregarDisabled(btn);
             borrarDuplicados(); 
 
@@ -134,31 +126,10 @@ function botonListener(string,element){
               })
         });       
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Me devuelve un array con la peli que devuelve true la función.
-
-// miListaJSON.filter(pelicu => pelicu.id === 1);
-
-
-
-
-
-//-------------- Agregar el disabled a las pelis que ya estan en mi lista
+            //Agregar el disabled a las pelis que ya estan en mi lista
 function chequearAgregado(){
-    for(let i =1; i<catalogoPeliculas.peliculas.length;i++){
-        if(misPelis.peliculas.filter(pelicu => pelicu.id === i).length===1){
+    for(let i =1; i<peliculas.length;i++){
+        if(misPelis.filter(pelicu => pelicu.id === i).length===1){
     
             let bott = document.getElementById(`btn${i}`);
             agregarDisabled(bott);
@@ -171,34 +142,16 @@ function agregarDisabled(boton){
     boton.setAttribute("disabled", "true");
     boton.innerText="Agregado";
 }
-//--------------
-///---------------------------
 
-// function eventEliminarMiLista(string,element){
-
-// //    let eliminar = misPelis.peliculas.id
-//     for(){
-//         misPelis.peliculas[i].id ===
-//     }
-
-// }
-
-
-
-///--------
 function borrarDuplicados(){
-    
-    let arrayDuplicados = catalogoMisPeliculas.peliculas;
+    let arrayDuplicados = misPelis;
     let set = new Set( arrayDuplicados.map( JSON.stringify ) );
     let arraySinDuplicados = Array.from( set ).map( JSON.parse );
-    catalogoMisPeliculas.peliculas = arraySinDuplicados;
-
+    misPelis = arraySinDuplicados;
 }
-///-------
-// FUNCIONES MENOS IMPORTANTES //
-
 //Funcion Lista de usuario
 function listaUsuario(){
     const listaUsuario = document.getElementById("miListado");
     listaUsuario.innerHTML=`Lista de ${localStorage.getItem("userName")}`
 }
+
